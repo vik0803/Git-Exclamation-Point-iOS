e7de54c8508c581c3ea404c45c47f40edc51ca57
--replace-all <manit8525@gmail.com>
2013-11-14 21:10:40 -0800

    Style Fixes.

--- Git!/IDTRepositoryTableViewController.m
+++ Git!/IDTRepositoryTableViewController.m
@@ -27,6 +27,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.clearsSelectionOnViewWillAppear = NO;
     self.gitDirectories = [[NSMutableArray alloc]initWithCapacity:10];
     NSArray *array = [self arrayOfFoldersInFolder:[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"]];
     for (NSString *folder in array) {
@@ -63,20 +64,20 @@
     static NSString *CellIdentifier = @"cellReuseRepo";
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    IDTGitDirectory *gitDirectory = [self.gitDirectories objectAtIndex:indexPath.row];
+    IDTGitDirectory *gitDirectory = self.gitDirectories[indexPath.row];
     cell.textLabel.text = gitDirectory.name;
     
     return cell;
 }
--(NSArray*)arrayOfFoldersInFolder:(NSString *) folder {
-	NSFileManager *fm = [NSFileManager defaultManager];
-	NSArray* files = [fm contentsOfDirectoryAtPath:folder error:nil];
+-(NSArray *)arrayOfFoldersInFolder:(NSString *) folder {
+	NSFileManager *fileManager = [NSFileManager defaultManager];
+	NSArray* files = [fileManager contentsOfDirectoryAtPath:folder error:nil];
 	NSMutableArray *directoryList = [NSMutableArray arrayWithCapacity:10];
     
 	for(NSString *file in files) {
 		NSString *path = [folder stringByAppendingPathComponent:file];
 		BOOL isDir = NO;
-		[fm fileExistsAtPath:path isDirectory:(&isDir)];
+		[fileManager fileExistsAtPath:path isDirectory:(&isDir)];
 		if(isDir) {
 			[directoryList addObject:file];
 		}
@@ -88,20 +89,25 @@
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
-        IDTGitDirectory *gitObject = [self.gitDirectories objectAtIndex:indexPath.row];
-        [self.gitDirectories removeObject:gitObject];
-        [gitObject delete];
+        IDTGitDirectory *gitDirectory = self.gitDirectories[indexPath.row];
+        [self.gitDirectories removeObject:gitDirectory];
+        [gitDirectory delete];
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
+    }
 }
 
 #pragma mark - Navigation
+
+-(void)createNewRepo:(id)sender
+{
+    [self performSegueWithIdentifier:@"segueToCreateRepo" sender:self];
+}
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([segue.identifier isEqualToString:@"segueToFiles"]) {
         IDTFileViewController *fileVC = [segue destinationViewController];
-        IDTGitDirectory *gitDirectory = [self.gitDirectories objectAtIndex:[self.tableView indexPathForSelectedRow].row];
+        IDTGitDirectory *gitDirectory = self.gitDirectories[[self.tableView indexPathForSelectedRow].row];
         fileVC.gitDirectory = gitDirectory;
         
     } else if ([segue.identifier isEqualToString:@"segueToCreateRepo"]) {
@@ -110,8 +116,5 @@
     
 }
 
--(void)createNewRepo:(id)sender {
-    [self performSegueWithIdentifier:@"segueToCreateRepo" sender:self];
-}
 
 @end
