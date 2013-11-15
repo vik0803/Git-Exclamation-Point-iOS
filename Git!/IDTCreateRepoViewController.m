60376a55b0ee93278528ccbe03615ef50ff90d42
--replace-all <manit8525@gmail.com>
2013-11-14 20:25:18 -0800

    Give IDTCreateRepoViewController some love.

--- Git!/IDTCreateRepoViewController.m
+++ Git!/IDTCreateRepoViewController.m
@@ -11,14 +11,24 @@
 #import "IDTGitDirectory.h"
 #import "IDTRepositoryTableViewController.h"
 @interface IDTCreateRepoViewController () <UITextFieldDelegate>
-
+// Weather or not the repo we are creating is going to be cloned. Set to `YES` if the cloneURLTextField is filled.
 @property (weak, nonatomic) IBOutlet UISwitch *cloneSwitch;
+// The name is extracted from `cloneURLTextField`.
 @property (weak, nonatomic) IBOutlet UITextField *repoNameTextField;
+// The URL of the remote repository to clone.
 @property (weak, nonatomic) IBOutlet UITextField *cloneURLTextField;
+// Only shown if cloneSwitch is set to `YES`. Then is set to `YES`
 @property (weak, nonatomic) IBOutlet UISwitch *cloneWithCheckoutSwitch;
+// A simple label. We have this as a property because we sometomes need to hide it.
 @property (weak, nonatomic) IBOutlet UILabel *checkoutLabel;
+// As above.
 @property (weak, nonatomic) IBOutlet UILabel *barelyLabel;
+// Weather or not the cloned repo will be bare. Set to `NO`
 @property (weak, nonatomic) IBOutlet UISwitch *cloneBarelySwitch;
+// A textView used during a clone.
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+// We need this property so we can dynaimcly show/hide it.
+@property (weak, nonatomic) IBOutlet UILabel *consoleLable;
 @end
 
 @implementation IDTCreateRepoViewController
@@ -34,6 +44,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
     self.view.tintColor = [UIColor purpleColor];
     self.cloneSwitch.on = NO;
     self.cloneWithCheckoutSwitch.hidden = YES;
@@ -42,8 +53,10 @@
     self.barelyLabel.hidden = YES;
     [self.cloneSwitch addTarget:self action:@selector(cloneSwitchDidTurn) forControlEvents:UIControlEventValueChanged];
     self.repoNameTextField.placeholder = @"Type in the name of the Repo";
-    self.cloneURLTextField.placeholder = @"Type in the URL of the git repository to clone. Make sure to turn on the clone switch aswell";
+    self.cloneURLTextField.placeholder = @"Type in the URL of the git repository to clone.";
     self.cloneURLTextField.delegate = self;
+    self.textView.editable = NO;
+    self.consoleLable.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,44 +65,58 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)createRepo:(id)sender {
-    IDTGitDirectory *gitRepository = Nil;
+    IDTGitDirectory *gitRepository = nil;
+    [self.view endEditing:YES];
     if (self.cloneSwitch.on) {
         NSError *error = nil;
-        gitRepository = [IDTGitDirectory cloneWithName:self.repoNameTextField.text URL:[NSURL URLWithString:self.cloneURLTextField.text] barely:self.cloneBarelySwitch.on checkout:self.cloneWithCheckoutSwitch.on error:&error];
+        gitRepository = [IDTGitDirectory cloneWithName:self.repoNameTextField.text URL:[NSURL URLWithString:self.cloneURLTextField.text] barely:self.cloneBarelySwitch.on checkout:self.cloneWithCheckoutSwitch.on transferProgressBlock:^(const git_transfer_progress *transfer_progress) {
+            self.consoleLable.hidden = NO;
+            NSString *string = [NSString stringWithFormat:@"Received %d objects and indexed %d out of  a total of %d \n",transfer_progress->received_objects,transfer_progress->indexed_objects,transfer_progress->total_objects];
+            self.textView.text = [self.textView.text stringByAppendingString:string];
+        } checkoutProgressBlock:^(NSString *path, NSUInteger completedSteps, NSUInteger totalSteps) {
+            self.consoleLable.hidden = NO;
+            NSString *string = [NSString stringWithFormat:@"Checking out Path %@. \n Completed %lu out of %lu \n",path,(unsigned long)completedSteps,(unsigned long)totalSteps];
+            self.textView.text = [self.textView.text stringByAppendingString:string];
+        } error:&error];
+        
         if (error) {
             NSLog(@"error is %@",error);
             UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Failure" message:error.description delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles:@"OK", nil];
             [alertView show];
             return;
         }
-    }else {
+    } else {
         NSError *error = nil;
         NSString *path = [NSHomeDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"Documents/%@",self.repoNameTextField.text]];
-        [[NSFileManager defaultManager]createDirectoryAtPath:path withIntermediateDirectories:NO attributes:Nil error:&error];
+        [[NSFileManager defaultManager]createDirectoryAtPath:path withIntermediateDirectories:NO attributes:nil error:&error];
         if (error) {
             NSLog(@"error is %@",error);
         }
-        BOOL success = [GTRepository initializeEmptyRepositoryAtURL:[NSURL fileURLWithPath:path] bare:NO error:&error];
-        if (success) {
-            GTRepository *repo = [[GTRepository alloc]initWithURL:[NSURL fileURLWithPath:path] error:&error];
+        GTRepository *repo = [GTRepository initializeEmptyRepositoryAtFileURL:[NSURL fileURLWithPath:path] error:nil];
+        if (repo) {
             gitRepository = [[IDTGitDirectory alloc]initWithRepo:repo];
         } else {
             NSLog(@"error is %@",error);
         }
     }
+    
     UISplitViewController *splitView = (UISplitViewController *)self.presentingViewController;
-    UINavigationController *navController = [splitView.viewControllers objectAtIndex:0];
-    IDTRepositoryTableViewController *repoTableVC = [navController.viewControllers objectAtIndex:0];
+    IDTRepositoryTableViewController *repoTableViewController = nil;
+    if (UIDeviceOrientationIsLandscape([UIDevice currentDevice].orientation)) {
+        UINavigationController *navigationController = splitView.viewControllers[0];
+        repoTableViewController = (IDTRepositoryTableViewController *)navigationController.topViewController;
+    } else {
+        repoTableViewController = splitView.viewControllers[0];
+    }
     [self dismissViewControllerAnimated:YES completion:^{
-        [repoTableVC.gitDirectories addObject:gitRepository];
-        [repoTableVC.tableView reloadData];
+        [repoTableViewController.gitDirectories addObject:gitRepository];
+        [repoTableViewController.tableView reloadData];
     }];
 
 }
 - (IBAction)cancel:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
-
 
 -(void)cloneSwitchDidTurn {
     if (self.cloneSwitch.on) {
@@ -109,19 +136,17 @@
 
 - (void)textFieldDidEndEditing:(UITextField *)textField {
     NSURL *url = [[NSURL alloc]initWithString:textField.text];
-    NSURLComponents *components = [[NSURLComponents alloc]initWithURL:url resolvingAgainstBaseURL:NO];
-    NSRegularExpression *regex = [[NSRegularExpression alloc]initWithPattern:@"[^/]+(?=/$|$)" options:0 error:nil];
-    [regex enumerateMatchesInString:components.path options:0 range:NSMakeRange(0, components.path.length) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
-        NSRange removeExtraneousCharactersRanger = result.range;
-        removeExtraneousCharactersRanger.length = removeExtraneousCharactersRanger.length - 4;
-        self.repoNameTextField.text = [components.path substringWithRange:removeExtraneousCharactersRanger];
-    }];
-    self.cloneSwitch.on = YES;
-    [self cloneSwitchDidTurn];
+    if (url) {
+        NSURLComponents *components = [[NSURLComponents alloc]initWithURL:url resolvingAgainstBaseURL:NO];
+        NSRegularExpression *regex = [[NSRegularExpression alloc]initWithPattern:@"[^/]+(?=/$|$)" options:0 error:nil];
+        [regex enumerateMatchesInString:components.path options:0 range:NSMakeRange(0, components.path.length) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+            NSRange removeExtraneousCharactersRanger = result.range;
+            removeExtraneousCharactersRanger.length = removeExtraneousCharactersRanger.length - 4;
+            self.repoNameTextField.text = [components.path substringWithRange:removeExtraneousCharactersRanger];
+        }];
+        self.cloneSwitch.on = YES;
+        [self cloneSwitchDidTurn];
+    }
 }
-
-
-
-
 
 @end
