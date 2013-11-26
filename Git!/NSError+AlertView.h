ebd658289dad8b0d6b187e2267ab0a4f34687f98
--replace-all <manit8525@gmail.com>
2013-11-26 15:58:04 -0800

    Adds an NSError category.

---
+++ Git!/NSError+AlertView.h
@@ -0,0 +1,15 @@
+//
+//  NSError+NSError_AlertView.h
+//  Git!
+//
+//  Created by E&Z Pierson on 11/15/13.
+//  Copyright (c) 2013 E&Z Pierson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface NSError (NSError_AlertView)
+
+-(void)showErrorInAlertView;
+
+@end 
---
+++ Git!/NSError+AlertView.m
@@ -0,0 +1,22 @@
+//
+//  NSError+NSError_AlertView.m
+//  Git!
+//
+//  Created by E&Z Pierson on 11/15/13.
+//  Copyright (c) 2013 E&Z Pierson. All rights reserved.
+//
+
+#import "NSError+AlertView.h"
+
+@implementation NSError (NSError_AlertView)
+
+-(void)showErrorInAlertView {
+    if (!self) {
+        return;
+    }
+    NSLog(@"error is %@",self);
+    UIAlertView *alerView = [[UIAlertView alloc]initWithTitle:@"Error" message:[self description] delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+    [alerView show];
+}
+
+@end
