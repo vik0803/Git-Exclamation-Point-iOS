551b11bd7de808789a9a7daf2dbbece46e5ad400
--replace-all <manit8525@gmail.com>
2013-12-15 22:59:11 -0800

    Improvements to the (still unsatisfactory) diff engine.

--- Git!/IDTDiffViewController.m
+++ Git!/IDTDiffViewController.m
@@ -15,79 +15,82 @@
 /// The individual file delta.
 @property (nonatomic, strong) GTDiffDelta *diffDelta;
 
-@property (nonatomic, strong) IDTDocument *currentDocument;
-
 @end
 
 @implementation IDTDiffViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-     
-    }
-    return self;
-}
-
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-   
-    self.tabBarController.title = @"HELLO";
-    self.currentDocument = [[IDTDocument alloc]initWithFileURL:[self.repo.fileURL URLByAppendingPathComponent:self.statusDelta.newFile.path]];
     [self determineDelta];
-    NSError *error = nil;
-    GTObjectDatabase *objectDatabase = [[GTObjectDatabase alloc]initWithRepository:self.repo error:&error];
-    if (error) {
-        NSLog(@"error is %@",error);
-    }
-    NSString *oldFileText = nil;
-    GTOdbObject *odbObject = [objectDatabase objectWithOID:self.statusDelta.oldFile.OID error:&error];
-    if (odbObject.data) {
-        oldFileText = [NSString stringWithUTF8String:[odbObject.data bytes]];
-    } else {
-        oldFileText = @"You are viewing this text because:\n 1. This 'file' is a submodule. (Submodule support is not implementaed, currently)\n 2. The file was deleted";
-    }
-
-    self.oldTextView.text = oldFileText;
-
-    [self.currentDocument openWithCompletionHandler:^(BOOL success) {
-        if (success) {
-            self.newTextView.text = self.currentDocument.userText;
-            [self.diffDelta enumerateHunksUsingBlock:^(GTDiffHunk *hunk, BOOL *stop) {
-                NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc]initWithString:self.newTextView.text];
-                [hunk enumerateLinesInHunk:nil usingBlock:^(GTDiffLine *line, BOOL *stop) {
-                    [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor blueColor] range:[self.newTextView.text rangeOfString:line.content]];
-                }];
-            }];
-        } else {
-            self.newTextView.text = @"You are viewing this text because:\n 1. This 'file' is a submodule. (Submodule support is not implemented, currently.)\n 2. The file was deleted";
-        }
-     }];
+    [self.diffDelta enumerateHunksUsingBlock:^(GTDiffHunk *hunk, BOOL *stop) {
+        NSMutableArray *lines = [NSMutableArray array];
+       [hunk enumerateLinesInHunk:nil usingBlock:^(GTDiffLine *line, BOOL *stop) {
+           [lines addObject:line];
+       }];
+        NSArray *array = [self diff:lines];
+        self.newTextView.attributedText = array[0];
+        self.oldTextView.attributedText = array[1];
+    }];
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
--(void)determineDelta {
+- (void)determineDelta {
     NSError *error = nil;
     GTDiff *diff = [GTDiff diffIndexToWorkingDirectoryInRepository:self.repo options:nil error:&error];
     if (error) {
         NSLog(@"Error is %@",error);
     }
     [diff enumerateDeltasUsingBlock:^(GTDiffDelta *delta, BOOL *stop) {
-        NSString *oldFilePath = [self.repo.fileURL.path stringByAppendingPathComponent:delta.oldFile.path];
-        if ([oldFilePath isEqualToString:self.currentDocument.fileURL.path]) {
+        if ([delta.oldFile.path isEqualToString:self.statusDelta.oldFile.path] || [delta.newFile.path isEqualToString:self.statusDelta.newFile.path]) {
             self.diffDelta = delta;
             *stop = YES;
         }
     }];
-    
+}
 
+- (NSArray *)diff:(NSMutableArray *)lines {
+    NSMutableAttributedString *new = [[NSMutableAttributedString alloc]init];
+    NSMutableAttributedString *old = [[NSMutableAttributedString alloc]init];
+
+    for (GTDiffLine *line in lines) {
+        switch (line.origin) {
+            case GTDiffLineOriginContext: {
+                NSString *lineContent = line.content;
+                if ([line.content isEqualToString:@""]) {
+                    lineContent = @"\n";
+                }
+                [old.mutableString appendString:lineContent];
+                [new.mutableString appendString:lineContent];
+                break;
+            }
+            case GTDiffLineOriginNoEOFNewlineContext:
+                [old.mutableString appendString:line.content];
+                [new.mutableString appendString:line.content];
+                break;
+            case GTDiffLineOriginAddEOFNewLine:
+                [new.mutableString appendString:line.content];
+                break;
+            case GTDiffLineOriginDeleteEOFNewLine:
+                [old.mutableString appendString:line.content];
+                break;
+            case GTDiffLineOriginAddition:
+                [new.mutableString appendString:line.content];
+                [new addAttribute:NSForegroundColorAttributeName value:[UIColor greenColor] range:[new.string rangeOfString:line.content]];
+                break;
+            case GTDiffLineOriginDeletion:
+                [old.mutableString appendString:line.content];
+                [old addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:[old.string rangeOfString:line.content]];
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return @[new,old];
 }
 
 @end
