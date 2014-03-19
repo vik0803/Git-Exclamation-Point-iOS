b649c0fbacb8de7652790e5c5450a19b8953bb56
PiersonBro <manit8525@gmail.com>
2014-03-18 23:21:18 -0700

    Style Fixes for SplitViewController.

--- Git!/IDTSplitViewController.m
+++ Git!/IDTSplitViewController.m
@@ -14,24 +14,13 @@
 
 @implementation IDTSplitViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
     self.delegate = self;
 	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
@@ -42,13 +31,6 @@
 }
 
 - (void)splitViewController:(UISplitViewController *)svc willShowViewController:(UIViewController *)aViewController invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem {
-
-    
 }
-
-
-
-
-
 
 @end
